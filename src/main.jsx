7ab2536d176d76f9fe2c0ca7709b94d4e9c77e69
DanielJantjies <162917612+DanielJantjies@users.noreply.github.com>
2025-06-14 08:29:23 +0200

    Error Handling, Components and npm files install

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'; // Import BrowserRouter
+import ReactDOM from 'react-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom' // Import BrowserRouter
 import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-     <App />
+      <App />
     </BrowserRouter>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
