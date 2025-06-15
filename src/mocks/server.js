40b8fed42730d18860976a4ac4fe9630d5da8427
DanielJantjies <162917612+DanielJantjies@users.noreply.github.com>
2025-06-15 16:02:07 +0200

    Host ID's

--- src/mocks/server.js
+++ src/mocks/server.js
@@ -15,12 +15,29 @@ export default function startMockServer() {
         server.create("van", { id: "6", name: "Green Wonder", price: 70, description: "With this van, you can take your travel life to the next level. The Green Wonder is a sustainable vehicle that's perfect for people who are looking for a stylish, eco-friendly mode of transport that can go anywhere.", imageUrl: "https://assets.scrimba.com/advanced-react/react-router/green-wonder.png", type: "rugged" })
     },
 
-    routes() {
-      this.namespace = "api";
-      this.get("/vans", (schema) => schema.vans.all());
-      this.get("/vans/:id", (schema, request) => {
-        return schema.vans.find(request.params.id);
-      });
+   routes() {
+        this.namespace = "api"
+        this.logging = false
+
+        this.get("/vans", (schema, request) => {
+            return schema.vans.all()
+        })
+
+        this.get("/vans/:id", (schema, request) => {
+            const id = request.params.id
+            return schema.vans.find(id)
+        })
+
+        this.get("/host/vans", (schema, request) => {
+            // Hard-code the hostId for now
+            return schema.vans.where({ hostId: "123" })
+        })
+
+        this.get("/host/vans/:id", (schema, request) => {
+            // Hard-code the hostId for now
+            const id = request.params.id
+            return schema.vans.findBy({ id, hostId: "123" })
+        })
     }
-  });
+})
 }
