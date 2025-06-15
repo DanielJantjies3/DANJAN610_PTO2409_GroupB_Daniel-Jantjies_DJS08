af48785b94f3b60a149f1ca61c7f57ace86b7c49
DanielJantjies <162917612+DanielJantjies@users.noreply.github.com>
2025-06-15 16:25:10 +0200

    Debugging

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import Home from "./Pages/Home" 
 import About from "./Pages/About" 
 import Vans from './Pages/Vans/vans'  
@@ -8,29 +8,26 @@ import Dashboard from './Pages/Host/Dashboard'
 import Income from './Pages/Host/Income'
 import Reviews from './Pages/Host/Reviews'
 import HostVans from './Pages/Host/HostVans'
-import HostVanDetail from './Pages/Host/HostVanDetail'
-import Layout from './components/Layout'  
-import HostLayout from './components/HostLayout'  
+import HostVanDetai from './Pages/Host/HostVanDetail'
+import Layout from '../components/Layout'
+import HostLayout from '../components/HostLayout'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-      
-        <Route element={<Layout />}>
+        <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="about" element={<About />} />
           <Route path="vans" element={<Vans />} />
           <Route path="vans/:id" element={<VanDetail />} />
-        </Route>
-        
-        
-        <Route path="host" element={<HostLayout />}>
-          <Route index element={<Dashboard />} />
-          <Route path="income" element={<Income />} />
-          <Route path="reviews" element={<Reviews />} />
-          <Route path="vans" element={<HostVans />} />
-          <Route path="vans/:id" element={<HostVanDetail />} />
+          
+          <Route path="host" element={<HostLayout />}>
+            <Route index element={<Dashboard />} />
+            <Route path="income" element={<Income />} />
+            <Route path="reviews" element={<Reviews />} />
+            <Route path="vans" element={<HostVans />} />
+          </Route>
         </Route>
       </Routes>
     </BrowserRouter>
