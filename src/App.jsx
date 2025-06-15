b390a953eb04813dea7302b6221db5b9c4cde28f
DanielJantjies <162917612+DanielJantjies@users.noreply.github.com>
2025-06-15 06:55:00 +0200

    Route Path

--- src/App.jsx
+++ src/App.jsx
@@ -2,27 +2,25 @@ import React from 'react'
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import Home from "./Pages/Home" 
 import About from "./Pages/About" 
-import Vans from './Pages/vans'
-
+import Vans from './Pages/vans'  
 
 import "/server"
 
 function App() {
   return (
     <BrowserRouter>
-
-<header>
-  <Link className="site-logo" to="/">#VANLIFE</Link> 
-  <nav>
-    <Link to="/About">About</Link>
-    <Link to="/vans">Vans</Link>
-  </nav>
-</header>
+      <header>
+        <Link className="site-logo" to="/">#VANLIFE</Link>
+        <nav>
+          <Link to="/about">About</Link>  
+          <Link to="/vans">Vans</Link>    
+        </nav>
+      </header>
       
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/About" element={<About />}/>
-        <Route path="/Vans" element={<Vans />} />
+        <Route path="/about" element={<About />}/>  
+        <Route path="/vans" element={<Vans />} />   
       </Routes>
     </BrowserRouter>
   )
