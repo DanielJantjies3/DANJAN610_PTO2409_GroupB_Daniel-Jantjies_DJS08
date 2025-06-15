04e3353b50d31313e1b2f98c97fb83c89a971893
DanielJantjies <162917612+DanielJantjies@users.noreply.github.com>
2025-06-15 16:13:00 +0200

    Routing Structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from "./Pages/Home" 
 import About from "./Pages/About" 
 import Vans from './Pages/Vans/vans'  
@@ -9,27 +9,28 @@ import Income from './Pages/Host/Income'
 import Reviews from './Pages/Host/Reviews'
 import HostVans from './Pages/Host/HostVans'
 import HostVanDetail from './Pages/Host/HostVanDetail'
-import Layout from '../components/Layout'
-import HostLayout from '../components/HostLayout'
+import Layout from './components/Layout'  
+import HostLayout from './components/HostLayout'  
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Layout />}>
+        {/* Main layout routes */}
+        <Route element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="about" element={<About />} />
           <Route path="vans" element={<Vans />} />
           <Route path="vans/:id" element={<VanDetail />} />
-          
-          <Route path="host" element={<HostLayout />}>
-            <Route index element={<Dashboard />} />
-            <Route path="income" element={<Income />} />
-            <Route path="reviews" element={<Reviews />} />
-            <Route path="vans" element={<HostVans />} />
-            <Route path="vans/:id" element={<HostVanDetail />} />
-
-          </Route>
+        </Route>
+        
+        {/* Host routes - separate layout */}
+        <Route path="host" element={<HostLayout />}>
+          <Route index element={<Dashboard />} />
+          <Route path="income" element={<Income />} />
+          <Route path="reviews" element={<Reviews />} />
+          <Route path="vans" element={<HostVans />} />
+          <Route path="vans/:id" element={<HostVanDetail />} />
         </Route>
       </Routes>
     </BrowserRouter>
